d807ac974002ac633dc243bd2d4b4b58596edd64
Amir-Golmoradi <LeoQueenzer2.0@gmail.com>
2024-11-24 02:00:52 +0330

    add: Project Backend

---
+++ Backend/CoachBackend/src/main/java/wisepup/coach/CoachBackendApplication.java
@@ -0,0 +1,13 @@
+package wisepup.coach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoachBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoachBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/CoachBackend/src/test/java/wisepup/coach/CoachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package wisepup.coach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
